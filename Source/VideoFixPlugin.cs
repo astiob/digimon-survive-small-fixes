ba1ca8f615ed96b7fc50ae0ff77c688f7fa7433c
Oleg Oshmyan <chortos@inbox.lv>
2023-02-01 01:55:00 +0200

    LF line endings, trailing line breaks, internally consistent indentation

--- Source/VideoFixPlugin.cs
+++ Source/VideoFixPlugin.cs
@@ -4,16 +4,16 @@ using HarmonyLib;
 namespace VideoFixPlugin
 {
     [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
-	public class VideoFixPlugin : BaseUnityPlugin
-	{
-		public void Awake()
-		{
+    public class VideoFixPlugin : BaseUnityPlugin
+    {
+        public void Awake()
+        {
             Logger.LogInfo($"Plugin {pluginGuid} loaded");
-			Harmony.CreateAndPatchAll(typeof(Patches));
-		}
+            Harmony.CreateAndPatchAll(typeof(Patches));
+        }
 
-		public const string pluginGuid = "mods.digimonsurvive.videofix";
+        public const string pluginGuid = "mods.digimonsurvive.videofix";
         public const string pluginName = "Video Fix Plugin";
-		public const string pluginVersion = "1.0.0.0";
-	}
+        public const string pluginVersion = "1.0.0.0";
+    }
 }
