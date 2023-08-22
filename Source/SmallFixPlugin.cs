56b853b760b27373c2ec244b7d3d90128e42d632
Oleg Oshmyan <chortos@inbox.lv>
2023-08-22 23:30:36 +0300

    Consistently use tabs for indentation in code

--- Source/SmallFixPlugin.cs
+++ Source/SmallFixPlugin.cs
@@ -3,17 +3,17 @@ using HarmonyLib;
 
 namespace SmallFixPlugin
 {
-    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
-    public class SmallFixPlugin : BaseUnityPlugin
-    {
-        public void Awake()
-        {
-            Logger.LogInfo($"Plugin {pluginGuid} loaded");
-            new Harmony(pluginGuid).PatchAll(typeof(Patches));
-        }
+	[BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+	public class SmallFixPlugin : BaseUnityPlugin
+	{
+		public void Awake()
+		{
+			Logger.LogInfo($"Plugin {pluginGuid} loaded");
+			new Harmony(pluginGuid).PatchAll(typeof(Patches));
+		}
 
-        public const string pluginGuid = "mods.digimonsurvive.smallfixes";
-        public const string pluginName = "Small Fix Plugin";
-        public const string pluginVersion = "1.1.0.0";
-    }
+		public const string pluginGuid = "mods.digimonsurvive.smallfixes";
+		public const string pluginName = "Small Fix Plugin";
+		public const string pluginVersion = "1.1.0.0";
+	}
 }
